3bd1f37807f3c70693ffaf25d3ad7c0c8d997d72
Nadyakilic <nadyakilic84@gmail.com>
2017-06-22 20:33:18 -0400

    Задание №2: Попытка 2.
    Не получается - Создаём в классе Point метод public double
    distance(Point p), согласно примеру, показанному в лекции 15. И вызываем
    метод из имеющегося класса main, вот таким образом p1.distance(p2).

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,24 +7,22 @@ public class MyFirstProgram {
     hello("user");
     hello("Alexey");
 
-    Square s= new Square(5);  //sozdali objekt square
-    System.out.println("Ploshad kvadrata so storonoj " + s.l+ "=" + s.area ());//atribut s.l
+    Square s = new Square(5);  //sozdali objekt square
+    System.out.println("Ploshad kvadrata so storonoj " + s.l + "=" + s.area());//atribut s.l
 
     Rectangle r = new Rectangle(4, 6);// chast koda v kotoroj sozdayutsya objektu.
-    System.out.println( "Ploshad pryamougolnika so storonami " + r.a + " i " +r.b + " = " + r.area());
+    System.out.println("Ploshad pryamougolnika so storonami " + r.a + " i " + r.b + " = " + r.area());
 
-    PointDistance d = new Distance();
-    System.out.println("The distance between the two points is " + distance + " .");
-
-
+    Point d = new Point (5, 6);
+    System.out.println("Rasstoyanie megdu dvumya tochkami" + d.x + "и" + d.y" =" + d.distance());
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello," + somebody + "!");
   }
-
-
 }
+
+
 
 
 
